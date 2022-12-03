4c6354bf046fcb9c04b07027d234f48609f8ceaa
bobby chan <bobbyc@bobbys-MacBook-Pro-2.local>
2022-12-03 09:45:36 -0800

    init app with public folder

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const app = express();
 const path = require('path')
-const port = process.env.PORT || 3001;
+const port = process.env.PORT;
 
+// template views
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+//static files
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req, res) => res.render('index', { title: "My App" }))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+});
 
 
